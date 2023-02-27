ffba973bef166981ab64f1f6dbfb4db4626a6c29
Joshua <rockjosh454@gmail.com>
2023-02-27 12:22:05 +0200

    bout to change to vite

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -7,10 +7,18 @@ import CartView from "@/views/CartView.vue";
 import FiltersView from "@/views/FiltersView.vue";
 import MenuView from "@/views/MenuView.vue";
 import FavouritesView from "@/views/FavouritesView.vue";
+import AuthenticateView from "@/views/authentication/AuthenticationView.vue";
+import LoginView from "@/views/authentication/LoginView.vue";
+import LoginVerifyView from "@/views/authentication/LoginVerifyView.vue";
 
 const routes = [
   {
     path: "/",
+    name: "user",
+    component: AuthenticateView,
+  },
+  {
+    path: "/home",
     name: "home",
     component: HomeView,
   },
@@ -43,6 +51,16 @@ const routes = [
     path: "/favourites",
     name: "favourites",
     component: FavouritesView,
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: LoginView,
+  },
+  {
+    path: "/login-verify",
+    name: "loginVerify",
+    component: LoginVerifyView,
   },
 ];
 
---
+++ frontend/src/views/authentication/AuthenticationView.vue
@@ -0,0 +1,36 @@
+<template>
+  <section id="container" class="h-screen w-screen">
+    <div class="w-full h-full flex-col flex">
+      <div class="w-full h-2/3 p-5">
+        <img src="@/assets/K-KART.png" alt="home" class="w-full h-full" />
+      </div>
+      <div class="w-full h-1/3 flex flex-col justify-center items-center gap-5">
+        <button
+          class="btn bg-blue-600 text-white text-lg"
+          @click="router.push('/login')"
+        >
+          Login
+        </button>
+        <!-- <button class="btn border border-blue-500 text-blue-500">
+          Get Started
+        </button> -->
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+</script>
+
+<style scoped>
+.btn {
+  @apply w-2/3 h-10 rounded-lg;
+}
+button:disabled {
+  background-color: rgba(88, 88, 234, 0.718);
+  color: rgb(166, 166, 248);
+}
+</style>
